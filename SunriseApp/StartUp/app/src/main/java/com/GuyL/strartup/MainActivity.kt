f8f4fee0098910d9c29f89d5b7288128c59d0872
Guy Levinger <guy123levinger@yahoo.com>
2017-06-28 14:25:22 +0300

    forgot brackets

--- SunriseApp/StartUp/app/src/main/java/com/GuyL/strartup/MainActivity.kt
+++ SunriseApp/StartUp/app/src/main/java/com/GuyL/strartup/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
     }
     protected fun getSunset(view:View){
         var city = cityName.text.toString()
-        val url ="https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22$city%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
+        val url ="https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22${city}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
         MyAsyncTask().execute(url)
 
     }
